512e3fcdeac66f55f3ef655e6517c8e5625a28a1
manish72 <manishguru72@gmail.com>
2023-08-01 21:05:06 +0530

    QueryBuilder concepts added

---
+++ aemdemo/core/src/main/java/com/aem/demo/core/services/SearchService.java
@@ -0,0 +1,9 @@
+package com.aem.demo.core.services;
+
+
+import org.json.JSONObject;
+
+public interface SearchService {
+
+	public JSONObject searchResult(String searchText,int startResult,int resultPerPage);
+}
---
+++ aemdemo/core/src/main/java/com/aem/demo/core/servlets/SearchServlet.java
@@ -0,0 +1,52 @@
+package com.aem.demo.core.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.servlets.SlingAllMethodsServlet;
+import org.apache.sling.servlets.annotations.SlingServletPaths;
+import org.json.JSONObject;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.aem.demo.core.services.SearchService;
+
+
+@Component(service = Servlet.class)
+@SlingServletPaths(
+		value = {"/demo/search"}
+)
+public class SearchServlet extends SlingAllMethodsServlet{	
+	private static final long serialVersionUID = 1L;	
+	private static final Logger LOG = LoggerFactory.getLogger(SearchServlet.class);
+	
+	@Reference
+	SearchService searchService;
+	
+	@Override
+	protected void doGet(final SlingHttpServletRequest request, final SlingHttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		JSONObject searchResult = null;
+		try {
+			String searchtext = request.getRequestParameter("searchtext").getString();
+			int pageNumber = Integer.parseInt(request.getRequestParameter("pageNumber").toString());
+			int resultPerPage = Integer.parseInt(request.getRequestParameter("resultPerPage").toString());
+			int startResult = pageNumber * resultPerPage;
+			searchResult = searchService.searchResult(searchtext,startResult,resultPerPage);
+		}
+		catch (Exception e) {
+			// TODO: handle exception
+			LOG.info("\n ERROR {} ",e.getMessage());
+		}
+		
+		response.setContentType("application/json");
+		response.getWriter().write(searchResult.toString());
+	}
+}
